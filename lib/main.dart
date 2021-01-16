81eff8f03e602072c4677cf9393412dcdfc1b09b
Josh Adams <joshua.william.adams@gmail.com>
2021-01-16 09:04:32 +0800

    Update main.dart
    default main.dart template

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,22 @@
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     /// Create application that uses Cupertino (iOS) design.
-//     return CupertinoApp(
-//       title: 'Pixabay API Search App',
-//       debugShowCheckedModeBanner: false,
+import 'package:flutter/cupertino.dart';
 
-//       /// Basic visual layout structure for single page iOS app.
-//       home: CupertinoPageScaffold(
-//         child: SafeArea(
-//           child: AppLayout(),
-//         ),
-//       ),
-//     );
-//   }
-// }
+void main() {
+  runApp(MyApp());
+}
 
-// class AppLayout extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     /// initialise business logic component (BLoC) for managing application state
-//     /// TODO - clean up block on dispose of widget
-//     PixabaySearchBloc pixabaySearchBloc = PixabaySearchBloc();
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'Pixabay API Search App',
+      debugShowCheckedModeBanner: false,
 
-//     return Column(
-//       mainAxisAlignment: MainAxisAlignment.center,
-//       mainAxisSize: MainAxisSize.max,
-//       crossAxisAlignment: CrossAxisAlignment.center,
-//       children: [
-//         Row(
-//           children: [
-//             Expanded(
-//               child: Container(
-//                 decoration: BoxDecoration(
-//                   border: Border(
-//                     bottom: BorderSide(
-//                       color: Colors.grey[400],
-//                     ),
-//                   ),
-//                 ),
-//                 child: Column(
-//                   children: [
-//                     /// Page title
-//                     Text('Pixabay'),
-
-//                     /// Page search input
-//                     Padding(
-//                       padding: const EdgeInsets.all(8.0),
-//                       child: CupertinoSearchTextField(
-//                         onChanged: (value) {
-//                           pixabaySearchBloc.getPictures(value);
-//                         },
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             )
-//           ],
-//         ),
-
-//         /// Scrollable search results
-//         Expanded(child: Container())
-//       ],
-//     );
-//   }
-// }
+      /// Basic visual layout structure for single page iOS app.
+      home: CupertinoPageScaffold(
+        child: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
